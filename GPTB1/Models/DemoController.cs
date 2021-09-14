8d99cb6a84394de5e05b42e605cc693f693ada74
LeThuTrang12 <lethutrang290400@gmail.com>
2021-09-14 09:20:29 +0700

    Add project files.

---
+++ GPTB1/Models/DemoController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GPTB1.Models
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GPT(string soA, string soB)
+        {
+            double soa = Convert.ToDouble(soA);
+            double sob = Convert.ToDouble(soB);
+            double ketqua = -sob / soa;
+            ViewBag.Giaipt = ketqua;
+            return View();
+        }
+    }
+}
---
+++ GPTB1/Models/LTQLDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace GPTB1.Models
+{
+    public partial class LTQLDbContext : DbContext
+
+    {
+        public LTQLDbContext() : base("name=LTQLDbContext")
+        {
+        }
+        public virtual DbSet<Account> Accounts { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+            .Property(e => e.Username)
+            .IsUnicode(false);
+        }
+    }
+}
+
+
+
